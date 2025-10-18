b48eb60b2fee5879f212c175c60147e66e197844
DinhTrongPhuc <dnhtrongphuc210104@gmail.com>
2025-10-19 00:21:06 +0700

    update 19/10/2025

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,15 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     private void Awake()
     {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
     }
 
     //ressources
@@ -35,28 +43,26 @@ public class GameManager : MonoBehaviour
 
     public void SaveState()
     {
-        Debug.Log("Save State");
-
         string s = "";
 
         s += "0" + "|";
         s += pessos.ToString() + "|";
-        s += experience + "|";
+        s += experience.ToString() + "|";
         s += "0";
 
         PlayerPrefs.SetString("SaveState",s);
     }
 
     public void LoadState(Scene s, LoadSceneMode mode)
-    {   if (PlayerPrefs.HasKey("SaveState"))
+    {   
+        if (!PlayerPrefs.HasKey("SaveState"))
             return;
 
         string[] data = PlayerPrefs.GetString("SaveState").Split('|');
-        // "0|10|15|2" => data 
 
         //change player skin
-        ///...
-        /// 
+        //...
+        
         pessos = int.Parse(data[1]);
         experience = int.Parse(data[2]);
 
